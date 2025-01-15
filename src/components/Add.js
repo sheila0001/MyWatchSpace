05137b4906c05b91928bf2546b5cd4db21e24c67
Sheila <sieyojisheila@gmail.com>
2025-01-14 20:05:24 -0500

    add a placeholder in add.js to browse movies and add react hooks to change text as user input

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+//to change the text as we type input-> use react hooks//
 
 export const Add = () => {
+    const [query, setQuery] = useState("");
+    const onChange = e => {
+        e.preventDefault();
+
+        setQuery(e.target.value);
+    };
     return (
-        <div>
-            <h1>Add Page</h1>
+        <div className='add-page'>
+            <div className='container'>
+                <div className='add-content'>
+                    <div className='input-wrapper'>
+                        <input type="text"
+                            placeholder="What movie are you looking for?"
+                            value={query}
+                            onChange={onChange}
+                        />
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
