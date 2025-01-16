f2cdb99da1a629f9beac84ce15964acc14b79395
Sheila <sieyojisheila@gmail.com>
2025-01-16 07:58:14 -0500

    fixed the bug on the action of adding to watched by removing a comma

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -7,18 +7,18 @@ export const MovieControls = ({ movie, type }) => {
         <div className="inner-card-controls">
             {type === 'watchlist' && (
                 <>
-                    <button className="ctrl-btn", onClick={() => addMovieToWatched(movie)}>
-                    <i className="fa-fw far fa-eye"></i>
-                </button>
+                    <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+                        <i className="fa-fw far fa-eye"></i>
+                    </button>
 
-            <button className="ctrl-btn"
-                onClick={() => removeMovieFromWatchlist(movie.id)}
-            >
-                <i className="fa-fw fa fa-times"></i>
-            </button>
-        </>
-    )
-}
+                    <button className="ctrl-btn"
+                        onClick={() => removeMovieFromWatchlist(movie.id)}
+                    >
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
+                </>
+            )
+            }
 
         </div >
     )
