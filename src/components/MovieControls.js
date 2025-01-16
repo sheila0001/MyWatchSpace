c2616a78f01a2d25351152ee929d3a44680a5339
Sheila <sieyojisheila@gmail.com>
2025-01-16 07:57:33 -0500

    add functionality of adding movies to watched page by clicking on the eye button

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -2,23 +2,24 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../CONTEXT/GlobalState";
 
 export const MovieControls = ({ movie, type }) => {
-    const { removeMovieFromWatchlist } = useContext(GlobalContext);
+    const { removeMovieFromWatchlist, addMovieToWatched } = useContext(GlobalContext);
     return (
         <div className="inner-card-controls">
             {type === 'watchlist' && (
                 <>
-                    <button className="ctrl-btn">
-                        <i className="fa-fw far fa-eye"></i>
-                    </button>
+                    <button className="ctrl-btn", onClick={() => addMovieToWatched(movie)}>
+                    <i className="fa-fw far fa-eye"></i>
+                </button>
 
-                    <button className="ctrl-btn"
-                        onClick={() => removeMovieFromWatchlist(movie.id)}
-                    >
-                        <i className="fa-fw fa fa-times"></i>
-                    </button>
-                </>
-            )}
+            <button className="ctrl-btn"
+                onClick={() => removeMovieFromWatchlist(movie.id)}
+            >
+                <i className="fa-fw fa fa-times"></i>
+            </button>
+        </>
+    )
+}
 
-        </div>
+        </div >
     )
 }
