446cf6198d2821a5492bc47ed7484cd1ced54b3b
Sheila <sieyojisheila@gmail.com>
2025-01-16 07:08:01 -0500

    copy watchlist.js page format to watched.js age

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -3,9 +3,25 @@ import { GlobalContext } from '../CONTEXT/GlobalState';
 import { MovieCard } from './MovieCard';
 
 export const Watched = () => {
+    const { watched } = useContext(GlobalContext);
     return (
-        <div>
-            <h1> Watched Page</h1>
+        <div className='movie-page'>
+            <div className='container'>
+                <div className='header'>
+                    <h1 className='heading'> Watched Movies</h1>
+                </div>
+                {watched.length > 0 ? (
+                    <div className='movie-grid'>
+                        {watched.map(movie => (
+                            <MovieCard movie={movie} type="watched" />
+                        ))}
+                    </div>
+                ) : (
+                    <h2 className="no-movies"> No movies in the Watched ...😟 </h2>
+                )}
+
+
+            </div>
         </div>
     );
 };
